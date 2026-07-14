af63695a6e2bd5e4f40415be1cb607c462883396
myvdy <misiewiczmichal4@gmail.com>
2026-07-14 19:44:25 +0200

    Setup CORS for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173", # defualt vite url
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = False,  # No cookies | Can change later when doing authentication
+    allow_methods = ["*"],    # * - GET, POST, DELETE etc.
+    allow_headers = ["Content-Type"],
+)
+
 
 @app.get("/health")
 def health_check():
