c7cfe5fc783161b0505490257b57f340c04c1212
123456789 <egzamin@poczta.pl>
2026-07-15 13:37:27 +0200

    Base tailwind configuration, setup of visual language

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <div style={{'display': 'flex', 'alignItems': 'center', 'justifyContent': 'center', 'textAlign': 'center', minHeight: '80vh', fontSize: '2rem', 'fontFamily': 'Arial, sans-serif', 'color': '#333', backgroundColor: '#f0f0f0'}}>
+      <div>
         <h1>Apprenticeship Journal</h1>
       </div>
     </>
