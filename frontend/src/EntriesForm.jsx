7fd4c6ab319a3cc10579938ab6931ac9b1f826e2
myvdy <misiewiczmichal4@gmail.com>
2026-07-23 13:41:10 +0200

    Fixed erorr status for frontend

--- frontend/src/EntriesForm.jsx
+++ frontend/src/EntriesForm.jsx
@@ -56,7 +56,7 @@ export default function EntriesForm({ userId, setCounter, setShowForm}) {
             return response.json().then((data) => {
                 if (!response.ok) {
                     const newErrors = {};
-                    if (data.code === "MISSING_REQUIRED_FIELDS" && data.details?.errors) {
+                    if (["MISSING_REQUIRED_FIELDS", "INVALID_FIELD_FORMAT"].includes(data.code) && data.details?.errors) {
                         const errs = data.details.errors;
                         if (errs.date) newErrors.date = errs.date;
                         if (errs.start_time) newErrors.startTime = errs.start_time;
